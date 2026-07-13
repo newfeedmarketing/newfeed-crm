086acd8d912011bfc256c090492b54f0a9a907e7
newfeedmarketing <newfeedmarketing24@gmail.com>
2026-07-13 17:54:34 -0300

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "10mb", // permite anexar contratos em PDF
+    },
+  },
+};
 
 export default nextConfig;
